6a33f5dc41b04461dcbef854119d63d5111faf69
Frank Garcia <garcia1901l@gmail.com>
2021-08-10 11:59:58 -0500

    create roles

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import morgan from 'morgan'
 
+import { createRoles } from './libs/initialSetup'
+
 import productRoutes from './routes/products.routes'
 import authRoutes from './routes/auth.routes'
 
 const app = express()
+
+createRoles();
 
 app.use(express.json())
 app.use(morgan('dev'))
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/User";
+import jwt from "jsonwebtoken";
+import config from "../config"
 
 export const register = async (req, res) => {
     const {username, email, password, roles} = req.body
@@ -9,9 +11,13 @@ export const register = async (req, res) => {
         password: await User.encryptPassword(password)
     })
 
-    await user.save()
+    const savedUser = await user.save()
 
-    res.json(user)
+    const token = jwt.sign({id: savedUser._id}, config.SECRET, {
+        expiresIn: 86400
+    })
+
+    res.json(token)
 }
 
 export const login = async (req, res) => {
